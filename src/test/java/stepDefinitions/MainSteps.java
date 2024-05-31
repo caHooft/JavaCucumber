35bb19e9b4b238ab954b495ff2c9dd55a74b25e9
Casper Hooft <casper.hooft@ordina.nl>
2024-05-31 11:16:42 +0200

    Cucumber tutorial start section 4
    Signed-off-by: Casper Hooft <casper.hooft@ordina.nl>

--- src/test/java/stepDefinitions/MainSteps.java
+++ src/test/java/stepDefinitions/MainSteps.java
@@ -9,6 +9,25 @@ import java.util.List;
 
 public class MainSteps
 {
-
+    @Given("User is on NetBanking landing page")
+    public void user_is_on_net_banking_landing_page() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("User is on NetBanking landing page");
+    }
+    @When("User login into application with <string> and password <string>")
+    public void user_login_into_application_with_string_and_password_string() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("User login into application");
+    }
+    @Then("Home Page is displayed")
+    public void home_page_is_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("Home Page is displayed");
+    }
+    @Then("Cards are displayed")
+    public void cards_are_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("Cards are displayed");
+    }
 
 }
