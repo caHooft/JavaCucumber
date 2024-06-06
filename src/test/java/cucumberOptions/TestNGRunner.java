aba2d1513ba74b07741f8457edcb9d3772770da4
Casper Hooft <casper.hooft@ordina.nl>
2024-06-06 16:30:27 +0200

    Cucumber tutorial section 6 Framework problems
    Signed-off-by: Casper Hooft <casper.hooft@ordina.nl>

--- src/test/java/cucumberOptions/TestNGRunner.java
+++ src/test/java/cucumberOptions/TestNGRunner.java
@@ -6,15 +6,13 @@ import io.cucumber.testng.CucumberOptions;
 //features and glue -> provides path to runner
 //monochrome makes test output more readable
 //tags makes it possible to only run tests with that tag
-//plugin ??
+//dryRun=true makes it possible for tests to keep going after error
 
 @CucumberOptions(features="src/test/java/features",
-glue="stepDefinitions",monochrome=true )
+//,dryRun=true
+glue="stepDefinitions",monochrome=true, plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json"})
         //,tags="SmokeTest or @RegressionTest"
-        //plugin = {"pretty", "html:target/cucumber.html","json:target/cucumber.json"} )
 public class TestNGRunner extends AbstractTestNGCucumberTests
 {
-
-	
 	
 }
